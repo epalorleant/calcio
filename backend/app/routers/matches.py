aab139259e7a92984ceff0fbe05a354decf7d008
Epal Njamen Orleant <epal.orleant@gmail.com>
2025-12-15 22:24:40 +0100

    fix backend

--- backend/app/routers/matches.py
+++ backend/app/routers/matches.py
@@ -74,7 +74,7 @@ def create_match(payload: schemas.MatchWithStatsCreate, db: Session = Depends(ge
     ratings.update_ratings_after_match(db, match)
     db.commit()
     db.refresh(match)
-    return _compose_session_match_response(match, session_players=session.session_players)
+    return match
 
 
 @router.get("/matches/{match_id}", response_model=schemas.MatchWithStatsRead)
