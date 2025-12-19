18ea0afdd32593422ae24ed5430e6c9ab86c089f
Epal Njamen Orleant <epal.orleant@gmail.com>
2025-12-19 17:43:45 +0100

    fix issue

--- frontend/src/pages/TemplatesPage.tsx
+++ frontend/src/pages/TemplatesPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import type { FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   createTemplate as createTemplateAPI,
